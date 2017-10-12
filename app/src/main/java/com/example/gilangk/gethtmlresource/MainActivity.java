1642193e1681263c9583b38e9bb8ef104b247715
cipow <cipoghost@gmail.com>
2017-10-12 18:08:34 +0700

    nambah isi spinner

--- app/src/main/java/com/example/gilangk/gethtmlresource/MainActivity.java
+++ app/src/main/java/com/example/gilangk/gethtmlresource/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.gilangk.gethtmlresource;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spin;
+    ArrayAdapter<CharSequence> list_spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spin = (Spinner) findViewById(R.id.spinner_protokol);
+        list_spinner = ArrayAdapter.createFromResource(this, R.array.protokol, android.R.layout.simple_spinner_item);
+        list_spinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(list_spinner);
+
     }
 }
